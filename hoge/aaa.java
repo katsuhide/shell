853f0e4df3f4570525ccb8e2ccc0604b809eaf12
Katsuhide <kamonegi.gg@gmail.com>
2012-05-02 00:05:24 +0900

    fix version

---
+++ hoge/aaa.java
@@ -0,0 +1,11 @@
+public abstract class HelloWorld {
+  public static void main (String[] args) {
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+    System.out.println("Hello World !!");
+  }
+} 
--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 # find the java file
-for file in `find . -name "*.java" | xargs grep -l "abstract" *`;
+for file in `find * -name "*.java" -type f | xargs grep -l abstract`;
 do 
     wc -l $file;
 done;
 
 # count the file rows
-
-
+rows=0;
+for file in `find * -name "*.java" -type f`;
+do 
+    tmp=`wc -l $file | awk '{print $1}'`;
+    rows=`expr $rows + $tmp`;
+done;
+echo $rows;
